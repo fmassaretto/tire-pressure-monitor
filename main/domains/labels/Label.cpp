00d6c6e67fbb5cc3364f9279d130082e98ffdd54
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-10-10 01:45:50 -0300

    Add lgvl_port_lock() to label methods

--- main/domains/labels/Label.cpp
+++ main/domains/labels/Label.cpp
@@ -16,6 +16,7 @@ void Label::init()
 
 void Label::setLabel(lv_obj_t *scr, lv_label_long_mode_t mode, unsigned int alignX, unsigned int alignY)
 {
+    lvgl_port_lock(0);
     // lv_obj_t *scr1 = lv_obj_create(NULL);
     // this->label = lv_label_create(scr1);
     this->label = lv_label_create(scr);
@@ -24,19 +25,26 @@ void Label::setLabel(lv_obj_t *scr, lv_label_long_mode_t mode, unsigned int alig
     lv_obj_set_width(label, 128);
     lv_obj_align(label, LV_ALIGN_TOP_MID, alignX, alignY);
     // lv_obj_clean(lv_scr_act());
+    lvgl_port_unlock();
 }
 
 void Label::setTextFormated(const char *fmt, float value)
 {
+    lvgl_port_lock(0);
     lv_label_set_text_fmt(label, fmt, value);
+    lvgl_port_unlock();
 }
 
 void Label::setText(char const *text)
 {
+    lvgl_port_lock(0);
     lv_label_set_text(label, text);
+    lvgl_port_unlock();
 }
 
 void Label::clearScreen()
 {
+    lvgl_port_lock(0);
     lv_obj_clean(lv_scr_act());
+    lvgl_port_unlock();
 }
