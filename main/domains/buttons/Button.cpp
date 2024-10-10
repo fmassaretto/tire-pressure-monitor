f3b15c6fe196769bb4b2083ea05155eacb046e8a
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-10-10 01:44:40 -0300

    Add single or long tap detection to button

--- main/domains/buttons/Button.cpp
+++ main/domains/buttons/Button.cpp
@@ -3,21 +3,52 @@
 Button::Button(gpio_num_t pin)
 {
     this->pin = pin;
+
     gpio_set_direction(pin, GPIO_MODE_INPUT);
+    gpio_set_intr_type(pin, GPIO_INTR_NEGEDGE);
 }
 
 Button::~Button()
 {
 }
 
+gpio_num_t Button::getPin()
+{
+    return this->pin;
+}
+
 press_type Button::getPressType() // TODO: return the right presstype
 {
-    int state = gpio_get_level(this->pin);
+    buttonState = gpio_get_level(this->pin);
+    // TickType_t currentTime = pdTICKS_TO_MS(xTaskGetTickCount());
 
-    if (state)
+    if (buttonState && !lastButtonState)
     {
+        buttonPressTime = esp_timer_get_time();
+        printf("buttonPressTime: %lu \n", buttonPressTime);
+    }
+
+    if (!buttonState && lastButtonState)
+    {
+        buttonReleaseTime = esp_timer_get_time();
+        printf("buttonReleaseTime: %lu \n", buttonReleaseTime);
+        pressDuration = buttonReleaseTime - buttonPressTime;
+        printf("pressDuration: %lu \n", pressDuration);
+
+        // lastButtonState = buttonState;
+    }
+
+    lastButtonState = buttonState;
+
+    if (pressDuration >= longPressThreshold)
+    {
+        pressDuration = 0;
+
         return LONG_PRESS;
     }
 
+    pressDuration = 0;
+
     return SINGLE_TAP;
+    // return SINGLE_TAP;
 }
--- main/domains/buttons/Button.h
+++ main/domains/buttons/Button.h
@@ -3,6 +3,8 @@
 #define _BUTTON_H_
 
 #include "driver/gpio.h"
+#include "esp_timer.h"
+#include "stdio.h"
 
 enum
 {
@@ -17,10 +19,17 @@ class Button
 private:
     press_type pressState;
     gpio_num_t pin;
+    const unsigned long longPressThreshold = 2000;
+    bool buttonState = false;
+    bool lastButtonState = false;
+    unsigned long buttonPressTime = 0;
+    unsigned long buttonReleaseTime = 0;
+    unsigned long pressDuration = 0;
 
 public:
     Button(gpio_num_t pin);
     ~Button();
+    gpio_num_t getPin();
     press_type getPressType();
 };
 
