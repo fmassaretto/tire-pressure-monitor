9594a528ee2ca31c1dabfbe05bb94499c9b0159f
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-10-30 01:14:14 -0300

    implement simple, double and long tap detection

--- main/domains/buttons/Button.cpp
+++ main/domains/buttons/Button.cpp
@@ -17,38 +17,104 @@ gpio_num_t Button::getPin()
     return this->pin;
 }
 
-press_type Button::getPressType() // TODO: return the right presstype
+// press_type Button::getPressType() // TODO: return the right presstype
+// {
+//     buttonState = gpio_get_level(this->pin);
+//     // TickType_t currentTime = pdTICKS_TO_MS(xTaskGetTickCount());
+
+//     if (buttonState && !lastButtonState)
+//     {
+//         buttonPressTime = esp_timer_get_time();
+//         printf("buttonPressTime: %lu \n", buttonPressTime);
+//     }
+
+//     if (!buttonState && lastButtonState)
+//     {
+//         buttonReleaseTime = esp_timer_get_time();
+//         printf("buttonReleaseTime: %lu \n", buttonReleaseTime);
+//         pressDuration = buttonReleaseTime - buttonPressTime;
+//         printf("pressDuration: %lu \n", pressDuration);
+
+//         // lastButtonState = buttonState;
+//     }
+
+//     lastButtonState = buttonState;
+
+//     if (pressDuration >= longPressThreshold)
+//     {
+//         pressDuration = 0;
+//         printf("pressDuration: %lu \n", pressDuration);
+
+//         return LONG_PRESS;
+//     }
+
+//     pressDuration = 0;
+
+//     return SINGLE_TAP;
+// }
+
+bool Button::buttonState()
 {
-    buttonState = gpio_get_level(this->pin);
-    // TickType_t currentTime = pdTICKS_TO_MS(xTaskGetTickCount());
+    return gpio_get_level(this->pin);
+}
 
-    if (buttonState && !lastButtonState)
+press_type Button::getButtonEvent()
+{
+    static const uint32_t DOUBLE_GAP_MILLIS_MAX = 250;
+    static const uint32_t LONG_MILLIS_MIN = 800;
+    static uint32_t button_down_ts = 0;
+    static uint32_t button_up_ts = 0;
+    static bool double_pending = false;
+    static bool long_press_pending = false;
+    static bool button_down = false;
+
+    press_type button_event = NO_TAP;
+    uint32_t now = esp_timer_get_time();
+    // uint32_t now = HAL_GetTick();
+
+    // If state changed...
+    if (button_down != buttonState())
     {
-        buttonPressTime = esp_timer_get_time();
-        printf("buttonPressTime: %lu \n", buttonPressTime);
+        button_down = !button_down;
+        if (button_down)
+        {
+            // Timestamp button-down
+            button_down_ts = now;
+        }
+        else
+        {
+            // Timestamp button-up
+            button_up_ts = now;
+
+            // If double decision pending...
+            if (double_pending)
+            {
+                button_event = DOUBLE_TAP;
+                double_pending = false;
+            }
+            else
+            {
+                double_pending = true;
+            }
+
+            // Cancel any long press pending
+            long_press_pending = false;
+        }
     }
 
-    if (!buttonState && lastButtonState)
+    // If button-up and double-press gap time expired, it was a single press
+    if (!button_down && double_pending && now - button_up_ts > DOUBLE_GAP_MILLIS_MAX)
     {
-        buttonReleaseTime = esp_timer_get_time();
-        printf("buttonReleaseTime: %lu \n", buttonReleaseTime);
-        pressDuration = buttonReleaseTime - buttonPressTime;
-        printf("pressDuration: %lu \n", pressDuration);
-
-        // lastButtonState = buttonState;
+        double_pending = false;
+        button_event = SINGLE_TAP;
+    }
+    // else if button-down for long-press...
+    else if (!long_press_pending && button_down && now - button_down_ts > LONG_MILLIS_MIN)
+    {
+        button_event = LONG_TAP;
+        long_press_pending = false;
+        double_pending = false;
     }
 
-    lastButtonState = buttonState;
-
-    if (pressDuration >= longPressThreshold)
-    {
-        pressDuration = 0;
-
-        return LONG_PRESS;
-    }
-
-    pressDuration = 0;
-
-    return SINGLE_TAP;
-    // return SINGLE_TAP;
+    return button_event;
 }
--- main/domains/buttons/Button.h
+++ main/domains/buttons/Button.h
@@ -8,7 +8,8 @@
 
 enum
 {
-    LONG_PRESS = 0,
+    NO_TAP = 0,
+    LONG_TAP,
     SINGLE_TAP,
     DOUBLE_TAP
 };
@@ -19,18 +20,20 @@ class Button
 private:
     press_type pressState;
     gpio_num_t pin;
-    const unsigned long longPressThreshold = 2000;
-    bool buttonState = false;
+    const unsigned long longPressThreshold = 4000000;
+    // bool buttonState = false;
     bool lastButtonState = false;
     unsigned long buttonPressTime = 0;
     unsigned long buttonReleaseTime = 0;
     unsigned long pressDuration = 0;
+    bool buttonState();
 
 public:
     Button(gpio_num_t pin);
     ~Button();
     gpio_num_t getPin();
-    press_type getPressType();
+    // press_type getPressType();
+    press_type getButtonEvent();
 };
 
 #endif
