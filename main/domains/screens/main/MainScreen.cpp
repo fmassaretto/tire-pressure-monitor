dad042b8a6f631219d7d2f67aa7300a174689631
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2024-10-30 01:18:04 -0300

    descommenting labels to show text on screen

--- main/domains/screens/main/MainScreen.cpp
+++ main/domains/screens/main/MainScreen.cpp
@@ -24,7 +24,7 @@ void MainScreen::init(lv_obj_t *scr)
     label1.setLabel(scr, LV_LABEL_LONG_SCROLL_CIRCULAR, 0, 0);
     label2.setLabel(scr, LV_LABEL_LONG_SCROLL_CIRCULAR, 0, 16);
 
-    // labelMain3.setLabel(LV_LABEL_LONG_SCROLL_CIRCULAR, 0, 32);
+    label3.setLabel(scr, LV_LABEL_LONG_SCROLL_CIRCULAR, 0, 32);
     // labelMain4.setLabel(LV_LABEL_LONG_SCROLL_CIRCULAR, 0, 48);
 }
 
@@ -34,8 +34,8 @@ void MainScreen::create()
     setCurrentScreen(MAIN_SCREEN);
 
     label1.setTextFormated("%4.2f psi", pressure.getPressureInPsi());
-    // label1.setTextFormated("%6.2fC", temperature.getTemperature());
-    label2.setTextFormated("%2.0f bar", pressure.getPressureInBar());
+    label2.setTextFormated("%6.2fC", temperature.getTemperature());
+    label3.setTextFormated("%2.0f bar", pressure.getPressureInBar());
 }
 
 void MainScreen::show()
